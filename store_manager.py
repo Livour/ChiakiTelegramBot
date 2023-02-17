2a4327261d077ebc2182e0f57afd47d2aa84038f
Vamqueror <liorx444@gmail.com>
2023-02-17 10:12:20 +0200

    can now add item to the store via add-item

--- store_manager.py
+++ store_manager.py
@@ -1,3 +1,4 @@
+import json
 from json import load as json_load
 from os.path import exists as does_path_exists
 from telebot import TeleBot
@@ -8,8 +9,31 @@ from telebot.types import Message
 STORAGE_FILE = "store.json"
 
 
+# TODO add exceptions
+
+
 def initialize_all_store_commands(bot: TeleBot):
     initialize_commands_from_storage(bot)
+
+    @bot.message_handler(commands=["add-item"])
+    def add_new_item(message: Message):
+        bot.send_message(message.chat.id, "Great, what item do you want to add?")
+        bot.register_next_step_handler(message, new_item_name)
+
+    def new_item_name(message: Message):
+        if is_duplicate(message.text):
+            return bot.send_message(message.chat.id, "Oops, An item with the same name already exists")
+
+        bot.send_message(message.chat.id, "Finally, give me the value you want to assign to that item")
+        bot.register_next_step_handler(message, lambda value_message: new_item_value(value_message, message.text))
+
+    def new_item_value(message: Message, name):
+        add_to_storage(name, message.text)
+
+        bot.message_handler(commands=[name])(
+            lambda new_item_message: bot.send_message(new_item_message.chat.id, message.text))
+
+        bot.send_message(message.chat.id, "The item has been added")
 
 
 def initialize_commands_from_storage(bot: TeleBot):
@@ -19,8 +43,7 @@ def initialize_commands_from_storage(bot: TeleBot):
             pass
         return
 
-    with open(STORAGE_FILE, "r") as items:
-        data = json_load(items)
+    data = get_all_storage()
 
     for key, value in data.items():
         def handler(closure_value=value):
@@ -30,3 +53,20 @@ def initialize_commands_from_storage(bot: TeleBot):
             return actual_handler
 
         bot.message_handler(commands=[key])(handler())
+
+
+def is_duplicate(text):
+    data = get_all_storage()
+    return text in data.keys()
+
+
+def get_all_storage() -> dict:
+    with open(STORAGE_FILE, "r") as items:
+        return json_load(items)
+
+
+def add_to_storage(key, value):
+    data = get_all_storage()
+    data[key] = value
+    with open(STORAGE_FILE, "w") as items:
+        json.dump(data, items)
